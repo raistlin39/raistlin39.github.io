6299d85e6fd61f97afc0addbd596340691419d68
raistlin39 <151050869+raistlin39@users.noreply.github.com>
2024-02-02 17:57:59 +0300

    Update deneme.R

--- deneme.R
+++ deneme.R
@@ -1,57 +1,3 @@
-
-crossing(var1=0:2, var2=0:2, var3=0:2, var4=0:2, var5=0:2)
-
-dbinom(x=5, size= 5, prob = 1/3)
-combn(10,5, min)
-
-combn(c(1,2,3,4,5), 3)
-
-
-(m <- combn(10, 5, min))   # minimum value in each combination
-
-mm <- as.data.frame(combn(5, 3, function(x) matrix(x, 2, 3)))
-
-
-## Different way of encoding points:
-combn(c(1,1,1,1,2,2,2,3,3,4), 3, tabulate, nbins = 4, simplify = FALSE)
-
-## Compute support points and (scaled) probabilities for a
-## Multivariate-Hypergeometric(n = 3, N = c(4,3,2,1)) p.f.:
-table.mat(t(combn(c(1,1,1,1,2,2,2,3,3,4), 3, tabulate, nbins = 4)))
-
-## Assuring the identity
-for(n in 1:7)
-  for(m in 0:n) stopifnot(is.array(cc <- combn(n, m)),
-                          dim(cc) == c(m, choose(n, m)),
-                          identical(cc, combn(n, m, identity)) || m == 1)
-
-
-
-as<-crossing(var1 = c(0,1,2), var2 = c(0,1,2), var3 = c(0,1,2))
-
-expand(mtcars, nesting(vs, cyl, gear))
-
-factor(mtcars$cyl)
-
-##########
-fruits <- tibble(
-  type = c("apple", "orange", "apple", "orange", "orange", "orange"),
-  year = c(2010, 2010, 2012, 2010, 2011, 2012),
-  size = factor(
-    c("XS", "S", "M", "S", "S", "M"),
-    levels = c("XS", "S", "M", "L")
-  ),
-  weights = rnorm(6, as.numeric(size) + 2)
-)
-
-fruits%>%expand(type)
-fruits%>%expand(type, size, year)
-fruits %>% expand(type, size, full_seq(year, 1))
-all <- fruits %>% expand(type, size, year)
-fruits %>% dplyr::right_join(all)
-
-
-
 ###################
 #### UYGULAMA #####
 ###################
@@ -112,11 +58,7 @@ olasılıklar3<-na.omit(olasılıklar)
 
 
 
-require(gtools)
-df1 <- data.frame(permutations(n = 4, r = 4, v = 1:4)) %>% cbind(sample(1:24))
-colnames(df1) <- c("Apples", "Pears", "Lemons", "Oranges", "Happiness")
 
-df2 = data.frame(Apples = c(1, 3, 2, 4), Pears = c(4, 1, 1, 3), Lemons = c(2, 2, 3, 1), Oranges = c(3, 4, 4, 2))
 
 
 
